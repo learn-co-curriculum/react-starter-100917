94375ff207b8980cd1ea9f1c01764e20bc1259b5
alexgriff <alexalexgriffith@gmail.com>
2017-12-10 15:29:09 -0500

    ready for starter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-// const createElement = (type, props = {}, children) => {
-//   return {
-//     $$typeof: Symbol.for('react.element'),
-//     type: type,
-//     props: { ...props, children: children },
-//     ref: null
-//   };
-// };
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const Article = props => {
   return React.createElement('div', {}, [
@@ -14,46 +8,7 @@ const Article = props => {
   ]);
 };
 
-const NavBar = props => {
-  return React.createElement(
-    'div',
-    { className: `ui inverted ${props.color} menu` },
-    React.createElement(
-      'a',
-      { className: 'item' },
-      React.createElement('h2', { className: 'ui header' }, [
-        React.createElement('i', { className: `${props.icon} icon` }),
-        React.createElement('div', { className: 'content' }, props.title),
-        React.createElement(
-          'div',
-          { className: 'sub header' },
-          props.description
-        )
-      ])
-    )
-  );
-};
-
-// <div class="ui inverted orange menu">
-//     <a class='item'>
-//       <h2 class="ui header">
-//         <i class="paw icon"></i>
-//         <div class="content">
-//           ZooKeepr
-//         </div>
-//         <div class="sub header">
-//           Keep track of your animals
-//         </div>
-//       </h2>
-//     </a>
-//   </div>
-
 ReactDOM.render(
-  NavBar({
-    color: 'green',
-    title: 'Paintr',
-    icon: 'paint brush',
-    description: 'wow idk what this does'
-  }),
+  Article({ title: 'Some News', text: 'probably something bad...' }),
   document.getElementById('main')
 );
